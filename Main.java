8eb0c0688a5817b22d88ac39f71865cf2647b579
seoe5580 <seoe5580@wrdsb.ca>
2020-09-14 13:54:38 +0000

    Finished the Hello World assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * This program prints Hello World to the Screen
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +9,8 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Prints "Hello World!" to the Screen
+    System.out.println("Hello World");
     
   }
 }
